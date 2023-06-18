5567378912cd6f507ecbc019e3f99721a33b76dd
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-06-19 00:35:16 +0700

    style admin

--- src/layout/admin-layout/AdminLayout.tsx
+++ src/layout/admin-layout/AdminLayout.tsx
@@ -1,95 +1,62 @@
-import { useState } from 'react';
-import {
-	MenuFoldOutlined,
-	MenuUnfoldOutlined,
-	DesktopOutlined,
-	InboxOutlined,
-	AppstoreOutlined,
-	UserOutlined,
-} from '@ant-design/icons';
-import { MenuProps, Avatar, Badge, Space } from 'antd';
-import { Layout, Menu, Button, theme } from 'antd';
-const { Header, Sider, Content } = Layout;
+// import { useState } from 'react';
+import './AdminLayout.scss';
 import { Link } from 'react-router-dom';
 import { Outlet } from 'react-router-dom';
 
-type MenuItem = Required<MenuProps>['items'][number];
-function getItem(
-	label: React.ReactNode,
-	key: React.Key,
-	icon?: React.ReactNode,
-	children?: MenuItem[],
-	type?: 'group'
-): MenuItem {
-	return {
-		key,
-		icon,
-		children,
-		label,
-		type,
-	} as MenuItem;
-}
-
 const AdminLayout: React.FC = () => {
-	const [collapsed, setCollapsed] = useState(false);
-	const {
-		token: { colorBgContainer },
-	} = theme.useToken();
 	return (
-		<Layout>
-			<Sider trigger={null} collapsible collapsed={collapsed}>
-				<div className='demo-logo-vertical' />
-				<Menu
-					theme='dark'
-					mode='inline'
-					defaultSelectedKeys={['1']}
-					items={[
-						getItem(<Link to='/admin'>Thống kê</Link>, '0', <DesktopOutlined />),
-						getItem('Sản phẩm', 'sub1', <InboxOutlined />, [
-							getItem(<Link to='/admin/product'>Sản phẩm</Link>, '1'),
-							getItem(<Link to='/admin/product/add'>Tạo sản phẩm</Link>, '2'),
-							getItem('Option 7', '3'),
-							getItem('Option 8', '4'),
-						]),
-						getItem('Navigation Two', 'sub2', <AppstoreOutlined />, [
-							getItem('Option 9', '5'),
-							getItem('Option 10', '6'),
-							getItem('Submenu', 'sub3', null, [getItem('Option 11', '11'), getItem('Option 12', '12')]),
-						]),
-					]}
-				/>
-			</Sider>
-			<Layout>
-				<Header style={{ padding: 0, background: colorBgContainer }}>
-					<Space size={12}>
-						<Button
-							type='text'
-							icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-							onClick={() => setCollapsed(!collapsed)}
-							style={{
-								fontSize: '16px',
-								width: 64,
-								height: 64,
-							}}
-						/>
-						{/* Avatar */}
-						<Badge count={1}>
-							<Avatar shape='square' icon={<UserOutlined />} />
-						</Badge>
-					</Space>
-				</Header>
-				<Content
-					style={{
-						margin: '24px 16px',
-						padding: 24,
-						minHeight: 280,
-						background: colorBgContainer,
-					}}
-				>
+		<div className='main-container d-flex'>
+			{/* Siderbar */}
+			<div className='siderbar collapse' id='siderBar'>
+				<div className='header-box px-2 pt-3'>
+					<h1 className='fs-4'>
+						<span className='bg-white text-dark rouded shadow px-2 me-2'>Gacha</span>
+						<span className='text-white'>Shop</span>
+					</h1>
+				</div>
+				<hr style={{ color: '#fff' }} />
+				{/* <ul className='list-unstyled px-2'></ul> */}
+				<ul className='p-0'>
+					<li className='px-4'>
+						<Link to='/admin/dashboard'>Thống kê</Link>
+					</li>
+					<li className='px-4'>
+						<button
+							type='button'
+							data-bs-toggle='collapse'
+							data-bs-target='#product'
+							aria-expanded='false'
+							aria-controls='product'
+						>
+							Sản phẩm
+						</button>
+						<div className='collapse' id='product'>
+							kkk
+						</div>
+					</li>
+				</ul>
+			</div>
+			{/* Content */}
+			<div className='content container-fluid p-0'>
+				<div className='content-h border-md shadown-sm mb-2 p-2'>
+					<div className='page-header'>
+						<button
+							className='btn'
+							type='button'
+							data-bs-toggle='collapse'
+							data-bs-target='#siderBar'
+							aria-expanded='false'
+							aria-controls='siderBar'
+						>
+							<i className='fa-solid fa-bars-staggered'></i>
+						</button>
+					</div>
+				</div>
+				<div className='p-2'>
 					<Outlet />
-				</Content>
-			</Layout>
-		</Layout>
+				</div>
+			</div>
+		</div>
 	);
 };
 
