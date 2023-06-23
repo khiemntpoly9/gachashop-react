336600e5e435fc2c7cb13859a545759f3f13747e
Nguyễn Trung Khiêm <trungkhiem1412@gmail.com>
2023-06-23 21:05:13 +0700

    add base api

--- src/utils/newRequest.ts
+++ src/utils/newRequest.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const newRequest = axios.create({
-	baseURL: 'https://walrus-app-h879i.ondigitalocean.app/api/',
+	baseURL: 'https://gacha-shop-inqa8.ondigitalocean.app/api/',
 	// baseURL: 'http://localhost:8080/api/',
 	withCredentials: true,
 });
